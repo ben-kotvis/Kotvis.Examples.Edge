72993a46f8da1fd39b1bd22fad69f55e1b5e5f66
Ben Kotvis <Ben.Kotvis@insight.com>
2020-06-18 20:58:53 -0500

    Add project files.

---
+++ Kotvis.Examples.Edge.Model/Interfaces/IEdgeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotvis.Examples.Edge.Model.Interfaces
+{
+    public interface IEdgeService
+    {
+        Task SendMessageToOutput(string outputName, string output, params NameValue[] properties);
+    }
+}
---
+++ Kotvis.Examples.Edge.Model/NameValue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Kotvis.Examples.Edge.Model
+{
+    public class NameValue
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public static NameValue Create(string name, string value)
+        {
+            return new NameValue()
+            {
+                Name = name,
+                Value = value
+            };
+        }
+    }
+}
